56e2e6baec69cdeb318f95dc19d5227026c5182f
Mauricio H Nagaoka <mhnagaoka@gmail.com>
2020-07-12 02:37:49 -0300

    Handles Chrome attempts to open a socket before even a request is made

--- ESP8266_Control_Relay.ino
+++ ESP8266_Control_Relay.ino
@@ -91,48 +91,126 @@ void setup()
   Serial.println("/");
 }
 
-void loop() {
-  // Check if a client has connected
+void loop()
+{
   WiFiClient client = server.available();
-  if (!client) {
-    digitalWrite(LED, millis() >> 11 & 1);
-    return;
-  }
-
-  // Wait until the client sends some data
-  Serial.println("New client");
-  while (!client.available()) {
-    delay(1);
-  }
-
-  // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
-
-  // Match the request
-  if (request.indexOf("?r=0") != -1)
+  // wait for a client (web browser) to connect
+  if (client)
   {
-    updateRelay(LOW);
-    Serial.println("RELAY=OFF");
-    sendRedirect(&client, "/");
-  }
-  else if (request.indexOf("?r=1") != -1)
-  {
-    updateRelay(HIGH);
-    Serial.println("RELAY=ON");
-    sendRedirect(&client, "/");
-  } else if (request.indexOf("GET / ") != -1) {
-    sendPage(&client);
-  } else {
-    sendNotFound(&client);
-  }
+    Serial.println("\n[Client connected]");
+    String request = "";
+    unsigned long start = millis();
+    while (client.connected()) {
+      // read line by line what the client (web browser) is requesting
+      if (client.available())
+      {
+        String line = client.readStringUntil('\r');
+        if (request.length() == 0) {
+          request = line;
+        }
+        Serial.print(line);
+        // wait for end of client's request, that is marked with an empty line
+        if (line.length() == 1 && line[0] == '\n')
+        {
+          Serial.printf("Handling request: %s\n", request.c_str());
+          // Match the request
+          if (request.indexOf("?r=0") >= 0)
+          {
+            updateRelay(LOW);
+            Serial.println("RELAY=OFF");
+            sendRedirect(&client, "/");
+          }
+          else if (request.indexOf("?r=1") >= 0)
+          {
+            updateRelay(HIGH);
+            Serial.println("RELAY=ON");
+            sendRedirect(&client, "/");
+          } else if (request.indexOf("GET / ") >= 0) {
+            Serial.println("PAGE");
+            sendPage(&client);
+          } else {
+            Serial.println("NOT FOUND");
+            sendNotFound(&client);
+          }
+          break;
+        }
+      } else {
+        // Dummy Chrome socket
+        // https://github.com/esp8266/Arduino/issues/3735
+        unsigned long elapsed = millis() - start;
+        if (elapsed > 1000) {
+          client.stop();
+          break;
+        }
+      }
+    }
+    delay(1); // give the web browser time to receive the data
 
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
-  blink(100, 100, 2);
+    // close the connection:
+    //    client.stop();
+    Serial.println("[Client disonnected]");
+  }
+  digitalWrite(LED, millis() >> 11 & 1);
 }
+
+//void loop() {
+//  // Check if a client has connected
+//  WiFiClient client = server.available();
+//  if (!client) {
+//    digitalWrite(LED, millis() >> 11 & 1);
+//    return;
+//  }
+//
+//  // Wait until the client sends some data
+//  Serial.println("New client");
+////  while (!client.available()) {
+////    delay(1);
+////  }
+//
+//  // Read the first line of the request
+//  String request = client.readStringUntil('\r');
+//  Serial.println("Request: " + request);
+//
+//  // https://github.com/esp8266/Arduino/blob/92373a98370618dea09718010b30d311a97f3f25/libraries/ESP8266WiFi/examples/WiFiManualWebServer/WiFiManualWebServer.ino#L86
+//  // read/ignore the rest of the request
+//  // do not client.flush(): it is for output only, see below
+//  while (client.available()) {
+//    // byte by byte is not very efficient
+//    client.read();
+//  }
+//
+//  // Match the request
+//  if (request.indexOf("?r=0") != -1)
+//  {
+//    updateRelay(LOW);
+//    Serial.println("RELAY=OFF");
+//    sendRedirect(&client, "/");
+//  }
+//  else if (request.indexOf("?r=1") != -1)
+//  {
+//    updateRelay(HIGH);
+//    Serial.println("RELAY=ON");
+//    sendRedirect(&client, "/");
+//  } else if (request.indexOf("GET / ") != -1) {
+//    Serial.println("PAGE");
+//    sendPage(&client);
+//  } else if (request.length() == 0) {
+//    // Sometimes Chrome opens a connection but doesn't send any request
+//    // So we dump the connection.
+//    // https://github.com/esp8266/Arduino/issues/3735
+//    Serial.println("DUMMY");
+//    client.stop();
+//    return;
+//  } else {
+//    Serial.println("NOT FOUND");
+//    sendNotFound(&client);
+//  }
+//
+//  delay(1);
+//  Serial.println("Client disonnected");
+//  Serial.println("");
+//  blink(100, 100, 2);
+//}
 
 void blink(int h, int l, int c) {
   for (int i = 0; i < c; i++) {
